233c58a3cf086a506e3b7072824a303456eaa9f8
Cheoklate <gcheok88@gmail.com>
2024-03-28 08:42:11 +0800

    created global.d.ts file so we can import .md files. edited index.tsx file so it can display the homepage now. Removed charts/chart files temporarily. Installed markdown-to-jsx to import md files.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { lazy, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const Homepage = lazy(() => import("./components/blog/Blog"));
+const SignUpPage = lazy(() => import("./components/SignUp"));
+const SignInPage = lazy(() => import("./components/LogIn"));
+const Dashboard = lazy(() => import("./components/dashboard/Dashboard"));
+
+const rootElement = document.createElement("div");
+
+document.body.appendChild(rootElement);
+
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Homepage />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);  
